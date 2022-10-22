db6e437201388f0113eba25b363bae605d420b39
Clement Chan <32990754+slyq@users.noreply.github.com>
2022-10-21 22:48:50 -0700

    Restructure files, add naive dialogue system

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] private DialogueRunner dialogueRunner;
+
+    [SerializeField] private string[] characterStartNodes;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            dialogueRunner.StartDialogue(characterStartNodes[0]);
+        }
+    }
+}
