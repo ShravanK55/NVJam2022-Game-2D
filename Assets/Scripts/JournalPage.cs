013cd0a632e9f8d44bf35eea8dc6ed9504a47fab
Ryan Lau <rclau@usc.edu>
2022-10-22 14:08:55 -0700

    fixed journal button to close tabs

--- Assets/Scripts/JournalPage.cs
+++ Assets/Scripts/JournalPage.cs
@@ -2,24 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Journal : MonoBehaviour
+public class JournalPage : MonoBehaviour
 {
-    [SerializeField] private GameObject journal;
-    
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
-
-    // called when journal button is clicked
-    public void OnClick()
-    {
-        journal.SetActive(!journal.activeSelf);
-
+        
     }
 }
