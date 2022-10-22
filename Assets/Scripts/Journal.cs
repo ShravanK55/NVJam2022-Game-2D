245eaefb109bb26cf0a14c509c2ad66c1792a2f6
Ryan Lau <rclau@usc.edu>
2022-10-22 13:15:47 -0700

    added tabs to journal

--- Assets/Scripts/Journal.cs
+++ Assets/Scripts/Journal.cs
@@ -9,13 +9,13 @@ public class Journal : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("hi");
     }
 
     // called when journal button is clicked
