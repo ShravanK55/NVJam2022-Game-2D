a9f5467f99d582635e0d52102faccac5ab0042e4
Ryan Lau <rclau@usc.edu>
2022-10-22 17:34:37 -0700

    added main menu and credits

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject CreditsMenu;
+    public string SceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuButton();
+    }
+
+    public void PlayNowButton()
+    {
+        // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called SceneName etc.)
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SceneName);
+    }
+
+    public void CreditsButton()
+    {
+        // Show Credits Menu
+        MainMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        // Show Main Menu
+        MainMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+    }
+}
