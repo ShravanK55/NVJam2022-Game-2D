cb2297c21a48cade412b48bf34eca023972a06d8
Shravan Kumar <shravan.k55@protonmail.com>
2022-10-23 16:10:05 -0700

    Implemented initiating dialogue by clicking on the sprites.

--- Assets/InitiateDialogue.cs
+++ Assets/InitiateDialogue.cs
@@ -7,6 +7,9 @@ public class InitiateDialogue : MonoBehaviour
 {
     [SerializeField] private DialogueManager dialogueManager;
     [SerializeField] private string characterName;
+    [SerializeField] private GameObject journal;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject winScreen;
     
     // Start is called before the first frame update
     void Start()
@@ -25,6 +28,9 @@ public class InitiateDialogue : MonoBehaviour
 
     private void OnMouseDown()
     {
-        dialogueManager.StartDialogue(characterName);
+        if (journal && !journal.activeSelf && pauseMenu && !pauseMenu.activeSelf && winScreen && !winScreen.activeSelf)
+        {
+            dialogueManager.StartDialogue(characterName);
+        }
     }
 }
